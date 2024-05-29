5624b579811b085987d633791ded413d2d98d4e5
dzhyranov <v.dzhyranov@gmail.com>
2024-05-29 21:31:18 +0300

    fix

--- task.sh
+++ task.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
-mysqldump -u $DB_USER -p'$DB_PASSWORD' ShopDB --no-create-db --result-file=bkup_ShopDB.sql
+mysqldump -u $DB_USER -p$DB_PASSWORD ShopDB --no-create-db --result-file=bkup_ShopDB.sql
 
-mysql -u $DB_USER -p'$DB_PASSWORD' ShopDBReserve < bkup_ShopDB.sql
+mysql -u $DB_USER -p$DB_PASSWORD ShopDBReserve < bkup_ShopDB.sql
 
-mysqldump -u $DB_USER -p'$DB_PASSWORD' ShopDB --no-create-db --no-create-info --result-file=bkup_ShopDBDevelopment.sql
+mysqldump -u $DB_USER -p$DB_PASSWORD ShopDB --no-create-db --no-create-info --result-file=bkup_ShopDBDevelopment.sql
 
-mysql -u $DB_USER -p'$DB_PASSWORD' ShopDBDevelopment < bkup_ShopDBDevelopment.sql
+mysql -u $DB_USER -p$DB_PASSWORD ShopDBDevelopment < bkup_ShopDBDevelopment.sql
